e51d95ea9fdfcfe60691e6a399782bdf9ac5a776
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-22 21:04:45 +0600

    modal on off

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Modal />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+// open modal
+// close modal
+function Modal() {
+  const closeStyle = {
+    margin: "40px",
+    cursor: "pointer",
+    textAlign: "center",
+  };
+  const [modalState, setModalState] = useState(true);
+  return (
+    <>
+      <h2 style={closeStyle} onClick={() => setModalState((state) => !state)}>
+        X
+      </h2>
+      {modalState && (
+        <div className="modal">
+          <h3>This is our text</h3>
+        </div>
+      )}
+    </>
+  );
+}
